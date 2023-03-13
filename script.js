159b6e601abd3a011f8a34e12999a24105b2e049
Prasathvj <124970493+Prasathvj@users.noreply.github.com>
2023-03-13 12:32:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function submit(){
+    var res=document.getElementById("firstname").value;
+    console.log(res);
+
+    var res=document.getElementById("middlename").value;
+    console.log(res);
+
+    var res=document.getElementById("lastname").value;
+    console.log(res);
+
+    var res=document.getElementById("email").value;
+    console.log(res);
+
+    var res=document.getElementById("password").value;
+    console.log(res);
+
+}
